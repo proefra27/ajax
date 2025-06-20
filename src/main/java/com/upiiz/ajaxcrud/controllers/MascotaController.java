c9449c6ead20a404e2fb4a8e6d66848bcf888266
Efrain Arredondo <proefra27@gmail.com>
2025-06-20 14:21:29 -0600

    Funcion de actulizar

--- src/main/java/com/upiiz/ajaxcrud/controllers/MascotaController.java
+++ src/main/java/com/upiiz/ajaxcrud/controllers/MascotaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -74,5 +75,38 @@ public class MascotaController {
                     "mensaje","No se pudo eliminar la mascota"
             ));
         }
+    }
+
+    @GetMapping("/v1/api/mascota/actualizar/{id}")
+    public ResponseEntity<Map<String,Object>> mascotaActualizar(@PathVariable int id) {
+        MascotaModel mascota = mascotaService.findMascotaById(id);
+        return ResponseEntity.ok(Map.of(
+                "estado",1,
+                "mensaje","Mascota encontrada",
+                "mascota", mascota
+        ));
+    }
+
+    @PostMapping("/v1/api/mascota/actualizar/{id}")
+    public ResponseEntity<Map<String,Object>> mascotaActualizarDatos(@PathVariable Long id, @RequestBody Map<String,Object> objetoMascota) {
+        MascotaModel mascota = new MascotaModel(
+                objetoMascota.get("nombre").toString(),
+                Integer.parseInt(objetoMascota.get("edad").toString()),
+                Integer.parseInt(objetoMascota.get("raza").toString()),
+                objetoMascota.get("observaciones").toString()
+                );
+        mascota.setId(id);
+        if(mascotaService.update(mascota) > 0)
+            return ResponseEntity.ok(Map.of(
+                    "estado",1,
+                    "mensaje","Mascota actualizada correctamente",
+                    "mascota", mascota
+            ));
+        else
+            return ResponseEntity.ok(Map.of(
+                    "estado",0,
+                    "mensaje","Error: No se pudo actualizar la mascota",
+                    "mascota", objetoMascota
+            ));
     }
 }
--- target/classes/static/js/sistema/crud-mascota-ajax.js
+++ target/classes/static/js/sistema/crud-mascota-ajax.js
@@ -1,6 +1,7 @@
 //CRUD Usado AJAX
 //Variables globales
 let idMascotaEliminar=0;
+let idMascotaActualizar=0;
 
 function obtenerMascotas() {
     $.ajax({
@@ -80,11 +81,94 @@ function guardarMascota(){
     }
 }
 
+function textoSelect(id){
+    var raza=""
+    switch (id){
+        case 1: raza="Gran Perro";
+        break;
+        case 2: raza="Chihuahua";
+        break;
+        case 3: raza="Huske";
+        break;
+        case 4: raza="Dalmata";
+    }
+    return raza;
+}
 function seleccionarMascotaActualizar(id) {
+    //1.- Seleccionar el id a actualizar
+    idMascotaActualizar=id;
+    //2.- Consultar la API para obtener los datos de la mascota - GET
 
+    $.ajax({
+        method:"GET",
+        url: "/v1/api/mascota/actualizar/"+idMascotaActualizar,
+        data: {},
+        success: function( resultado ) {
+            if(resultado.estado===1){
+                let mascota = resultado.mascota;
+                $('#nombre_mascota_editar').val(mascota.nombre);
+                $('#edad_mascota_editar').val(mascota.edad);
+                //Como selecciono un elemento de un Select con JQuery?
+                //$('#raza_editar').val('Chihuahua').trigger('change.select2');
+                //alert($("#raza_editar option[value='2']").prop("selected", true));
+                $('#raza_editar option').each(function() {
+                    const valor = $(this).val();
+                    const texto = $(this).text();
+                    console.log(`Value: ${valor}, Texto: ${texto}`);
+                });
+                $('#raza_editar').val(3);
+                $('#comentarios_editar').val(mascota.observaciones);
+            }else{
+                alert(resultado.mensaje);
+            }
+        },
+        error:function (xhr,error, mensaje) {
+            alert(mensaje);
+        }
+    });
+    //3.- Mostrar los datos en el Modal
 }
 
 function actualizarMascota() {
+    //1.- Obtener los datos que existen en el modal
+    nombre_mascota=$('#nombre_mascota_editar').val();
+    edad_mascota=$('#edad_mascota_editar').val();
+    raza_mascota=$('#raza_editar').val();
+    observaciones_mascota=$('#comentarios_editar').val();
+    if(nombre_mascota && edad_mascota>0 && observaciones_mascota){
+        $.ajax({
+            url: "/v1/api/mascota/actualizar/"+idMascotaActualizar,
+            contentType:"application/json",
+            method:"POST",
+            data: JSON.stringify({
+                id:idMascotaActualizar,
+                nombre:nombre_mascota,
+                edad:edad_mascota,
+                raza:raza_mascota,
+                observaciones:observaciones_mascota
+            }),
+            success: function( resultado ) {
+                if(resultado.estado==1){
+                    let tabla = $('#example').DataTable();
+                    datos = tabla.row("#renglon_"+idMascotaActualizar).data()
+                    datos[1]=nombre_mascota;
+                    datos[2]=edad_mascota;
+                    tabla.row("#renglon_"+idMascotaActualizar).data(datos);
+                    tabla.draw()
+                    alert(resultado.mensaje);
+                }else{
+                    //Todo mal
+                    alert(resultado.mensaje);
+                }
+            },
+            error:function (xhr,error,mensaje) {
+                //Se dispara la funcion si no conexion al servidor
+                alert("Error de comunicacion: "+error);
+            }
+        });
+    }else{
+        alert("Ingresa los datos correctamente")
+    }
 
 }
 
